70dc83e9ce0a00a77dd944d612d514e8d9a4a9e3
aliyasini65 <60253718+aliyasini65@users.noreply.github.com>
2020-01-24 12:01:53 +0330

    Add files via upload

---
+++ fffff.py
@@ -0,0 +1,10 @@
+class team:
+    def __init__(self,nation,bestplayer):
+        self.nation=nation
+        self.bestplayer=bestplayer
+for i in range(1,500):
+ clubi=team("spanish","messi")
+print(club3) 
+
+
+
